25207e72b20f198dac1786b323260a24cb761d0f
barosanuemailtest <barosanu.email.test@gmail.com>
2020-06-01 14:02:08 +0300

    11 - Server implementation

--- src/Server/Server.ts
+++ src/Server/Server.ts
@@ -1,6 +1,14 @@
+import { createServer, IncomingMessage, ServerResponse } from 'http';
+
 export class Server {
 
     public createServer() {
-        console.log('created server');
+        createServer(
+            (req: IncomingMessage, res: ServerResponse) => {
+                console.log('got request from: ' + req.url);
+                res.end();
+            }
+        ).listen(8080);
+        console.log('server started')
     }
 }
