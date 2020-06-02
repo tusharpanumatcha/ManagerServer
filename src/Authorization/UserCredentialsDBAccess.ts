82e2d9668ab9534c8f3eafb41162c22e88214793
barosanuemailtest <barosanu.email.test@gmail.com>
2020-06-02 18:36:19 +0300

    20 - UserCreds DataBase

---
+++ src/Authorization/UserCredentialsDBAccess.ts
@@ -0,0 +1,17 @@
+import { UserCredentials } from "../Shared/Model";
+
+
+
+export class UserCredentialsDBAccess {
+
+
+
+    public async putUserCredential(userCredentials: UserCredentials): Promise<any> {
+
+    }
+
+    public async getUserCredential(username: string, password: string): Promise<UserCredentials | undefined> {
+
+    }
+
+}
