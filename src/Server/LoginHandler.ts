75ac296fb083f3793978f78cd63a81da6347f4b9
barosanuemailtest <barosanu.email.test@gmail.com>
2020-06-02 13:45:02 +0300

    17 - Interfaces

--- src/Server/LoginHandler.ts
+++ src/Server/LoginHandler.ts
@@ -1,6 +1,7 @@
 import { IncomingMessage, ServerResponse } from "http";
+import { Account, Handler } from "./Model";
 
-export class LoginHandler {
+export class LoginHandler implements Handler {
 
     private req: IncomingMessage;
     private res: ServerResponse;
@@ -14,10 +15,10 @@ export class LoginHandler {
         console.log('before getting Body');
         const body = await this.getRequestBody();
         console.log('request username: ' + body.username);
-        console.log('request password: ' + body.username);
+        console.log('request password: ' + body.password);
     }
 
-    private async getRequestBody(): Promise<any> {
+    private async getRequestBody(): Promise<Account> {
         return new Promise((resolve, reject) => {
             let body = '';
             this.req.on('data', (data: string) => {
---
+++ src/Server/Model.ts
@@ -0,0 +1,10 @@
+
+
+export interface Account {
+    username: string,
+    password: string
+}
+
+export interface Handler {
+    handleRequest(): void
+}
