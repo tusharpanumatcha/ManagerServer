6e94d4a090d4e5696ff20f0519d699f282c7d8f4
barosanuemailtest <barosanu.email.test@gmail.com>
2020-06-03 16:54:51 +0300

    26 - User DataBase

---
+++ src/User/UsersDBAccess.ts
@@ -0,0 +1,24 @@
+import * as Nedb from 'nedb';
+import { User } from '../Shared/Model';
+
+export class UsersDBAccess {
+
+    private nedb: Nedb;
+
+    constructor() {
+        this.nedb = new Nedb('database/Users.db');
+        this.nedb.loadDatabase();
+    }
+
+    public async putUser(user: User) {
+        return new Promise((resolve, reject) => {
+            this.nedb.insert(user, (err: Error) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve();
+                }
+            })
+        });
+    }
+}
--- test/DbTest.ts
+++ test/DbTest.ts
@@ -1,14 +1,24 @@
 import { UserCredentialsDBAccess } from "../src/Authorization/UserCredentialsDBAccess";
+import { UsersDBAccess } from "../src/User/UsersDBAccess";
 
 
 
 class DbTest {
 
     public dbAccess: UserCredentialsDBAccess = new UserCredentialsDBAccess();
+    public userDbAccess: UsersDBAccess = new UsersDBAccess();
 }
 
-new DbTest().dbAccess.putUserCredential({
-    username: 'user1',
-    password: 'password1',
-    accessRights: [1, 2, 3]
+// new DbTest().dbAccess.putUserCredential({
+//     username: 'user1',
+//     password: 'password1',
+//     accessRights: [1, 2, 3]
+// });
+
+new DbTest().userDbAccess.putUser({
+    age: 30,
+    email: 'some@email.com',
+    id: 'asd23234',
+    name: 'John Abc',
+    workingPosition: 3
 });
